4a27bf81e873c1b1e21cd559b6e2eaee1b5f221a
chunikhin80 <k-o-r@tut.by>
2019-06-20 22:36:11 +0300

    lesson1

---
+++ src/by/it/Java2/Lesson2/task9.java
@@ -0,0 +1,20 @@
+package by.it.Java2.Lesson2;
+
+import java.util.Scanner;
+
+public class task9 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int a=sc.nextInt();
+        int i;
+        for (i=2; i<a; i++){
+            if (a%i==0){
+                break;
+
+                    
+                }
+        }
+
+    }
+}
