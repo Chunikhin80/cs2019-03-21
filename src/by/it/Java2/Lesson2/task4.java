e76a04336c485550b9db9c937041218fc65fc3f1
chunikhin80 <k-o-r@tut.by>
2019-06-20 22:01:49 +0300

    lesson1

--- src/by/it/Java2/Lesson2/task4.java
+++ src/by/it/Java2/Lesson2/task4.java
@@ -5,10 +5,20 @@ public class task4 {
         int array[];
         array = new int[3];
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) Math.floor(Math.random() * 100)+10;
-            System.out.print(array[i] + "  ");
+            array[i] = diapazon();
+            System.out.print(array[i] + " ");
         }
-        if (array[2]>array[1] && array[1]>array[0]){
-        System.out.println("Это строго возрастающая последовательность");}
+        if (array[2]>array[1] && array[1] > array[0]){
+            System.out.println("\n" + "Это строго возрастающая последовательность");
+        }
     }
+    private static int diapazon() {
+        int max=99;
+        int min=10;
+        int r;
+        int z;
+        r= (max-min);
+        z= (int) Math.floor(Math.random() * ++r)+min;
+        return z;
+            }
 }
