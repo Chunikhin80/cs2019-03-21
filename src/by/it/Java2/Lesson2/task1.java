080a287b6077c2976e9fe74fd6153013532cb233
chunikhin80 <k-o-r@tut.by>
2019-06-20 16:21:20 +0300

    lesson1

--- src/by/it/Java2/Lesson2/task1.java
+++ src/by/it/Java2/Lesson2/task1.java
@@ -2,6 +2,13 @@ package by.it.Java2.Lesson2;
 
 public class task1 {
     public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
 
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);}
+        System.out.println(" ");
+
+        for (int i = array.length-1; i >= 0; i--) {
+            System.out.println(array[i]);}
     }
 }
