e7c83bf1afc14dde2eb59ca09b8f7c69cc171c14
chunikhin80 <k-o-r@tut.by>
2019-06-20 19:28:04 +0300

    lesson1

--- src/by/it/Java2/Lesson2/task2.java
+++ src/by/it/Java2/Lesson2/task2.java
@@ -2,20 +2,26 @@ package by.it.Java2.Lesson2;
 
 import java.util.Scanner;
 
-public class task2{
+public class task2 {
     public static void main(String[] args) {
-       int i=0;
-       int a=2;
-       int array[];
-       array = new int[10];
-       while (i<=array.length);{
-           if ((a % 2==0) || (a % 4==0)){
-               array[i]=a;}
-           i+=array[i];
-           a++;
-           }
-           System.out.println(array[i]);
-       }
+        int i = 0;
+        int a = 2;
+        int array[];
+        array = new int[10];
+        while (i<=9);
+        {       if ((a % 2 == 0) || (a % 4 == 0)) {
+                array[i] = a;
+                a++;
+                i++;}
+                else
+                a++;
+                i=i;
+
+            }
+        for (i=0; i<array.length; i++)
+            System.out.println(array[i]);
+        }
+
     }
 
 
