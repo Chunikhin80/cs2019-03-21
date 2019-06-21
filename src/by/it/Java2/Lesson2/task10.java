0057a48511c5f4317db430614669dabcf9f3f02a
Alexlifeby <1985alexlife>
2019-06-21 09:49:26 +0300

    Lesson 2

---
+++ src/by/it/Java2/Lesson2/task10.java
@@ -0,0 +1,15 @@
+package by.it.Java2.Lesson2;
+
+import java.util.Scanner;
+
+/**
+ * Created by user on 21.06.2019.
+ */
+public class task10 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Введите число ");
+        int a=sc.nextInt();
+        
+    }
+}
