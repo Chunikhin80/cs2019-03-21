e2845777a64452c1f60f3e458cfe959cc2a24b99
Alexlifeby <1985alexlife>
2019-06-21 10:21:03 +0300

    Lesson 2

--- src/by/it/Java2/Lesson2/task10.java
+++ src/by/it/Java2/Lesson2/task10.java
@@ -10,10 +10,17 @@ public class task10 {
         Scanner sc=new Scanner(System.in);
         System.out.println("Введите число ");
         int a=sc.nextInt();
-       for (a=0;a<=1;a++){
-           System.out.println("Факториал числа = 1");
-           break;
-       }
-        
+       //for (a=0;a<=1;a++){
+         //  System.out.println("Факториал числа = 1");
+           //break;}
+        int i=1;
+        int c;
+        while (i<a){
+            c=i*(i+1);
+            i++;
+            System.out.println("Факториал числа =" + c);
+
+            }
+
     }
 }
