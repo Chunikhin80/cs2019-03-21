45025accd060a9a57d49719e7a29b45101c1daaa
Alexlifeby <1985alexlife>
2019-06-21 09:24:44 +0300

    Lesson 2

--- src/by/it/Java2/Lesson2/task9.java
+++ src/by/it/Java2/Lesson2/task9.java
@@ -4,17 +4,19 @@ import java.util.Scanner;
 
 public class task9 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Введите число: ");
-        int a=sc.nextInt();
+        int a = sc.nextInt();
         int i;
-        for (i=2; i<a; i++){
-            if (a%i==0){
+        for (i = 2; i < a; i++) {
+            if (a % i == 0) {
+                System.out.println("Это не простое число ");
                 break;
+            } else {
+                System.out.println("Это простое число ");
+                break;
+            }
 
-                    
-                }
         }
-
     }
-}
+    }
