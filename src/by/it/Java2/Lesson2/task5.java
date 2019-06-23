15a2d8736b947607dd5bf2f597f9229dce89a3ed
chunikhin80 <k-o-r@tut.by>
2019-06-23 18:48:41 +0300

    lesson1

--- src/by/it/Java2/Lesson2/task5.java
+++ src/by/it/Java2/Lesson2/task5.java
@@ -2,6 +2,12 @@ package by.it.Java2.Lesson2;
 
 public class task5 {
     public static void main(String[] args) {
-        
+        int[][] array = {{1, 1, 1, 1, 1}, {0, 1, 1, 1, 0}, {0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {1, 1, 1, 1, 1}};
+        int i;
+        int j;
+        for (i = 0; i < array.length; i++){
+        for (j=0; j<array[i].length; j++){
+        System.out.print(array[i][j] + " ");}
+        System.out.println();}
     }
 }
