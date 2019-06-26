57f4116eef8da5f52fdcfc6252f64eedbfbbd352
Alexlifeby <1985alexlife>
2019-06-26 10:38:46 +0300

    lesson2

--- src/by/it/Java2/Lesson2/task6.java
+++ src/by/it/Java2/Lesson2/task6.java
@@ -4,27 +4,23 @@ public class task6 {
     public static void main(String[] args) {
 
 
-        int[][] array1 = {{4, 6}, {7, 8}};
-        int[][] array2 = {{4, 3}, {2, 1}};
+        int[][] array1 = {{4, 2}, {7, 9}};
+        int[][] array2 = {{4, 3}, {3, 6}};
 
         int j = 0;
         int i = 0;
-        int a = 0;
-        int b = 0;
-        if (array1[i][j] == array2[a][b]) {
-            System.out.println("Соответствующие элементы равны ");
-            i++;
-            j++;
-            a++;
-            b++;
+        int s = 0;
+        for (i = 0; i < array1.length; i++) {
+            for (j = 0; j < array1[i].length; j++) {
+                if (array1[i][j] == array2[i][j]) {
+                    s = s + array1[i][j];
+                }
+            }
         }
+        if (s > 0)
+            System.out.println("Соответствующие элементы равны");
         else
-            System.out.println("Соответствующи элементы не равны");
-
-
-
+            System.out.println("Соответствующие элементы не равны ");
     }
-
 }
-
 
