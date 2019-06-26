200840bd3be1b7321165d38a66ec130ebf738c83
Alexlifeby <1985alexlife>
2019-06-26 10:57:52 +0300

    lesson2

--- src/by/it/Java2/Lesson2/task5.java
+++ src/by/it/Java2/Lesson2/task5.java
@@ -5,8 +5,11 @@ public class task5 {
         int[][] array = {{1, 1, 1, 1, 1}, {0, 1, 1, 1, 0}, {0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {1, 1, 1, 1, 1}};
         int i;
         int j;
+        ;
         for (i = 0; i < array.length; i++){
         for (j=0; j<array[i].length; j++){
+            
+
         System.out.print(array[i][j] + " ");}
         System.out.println();}
     }
