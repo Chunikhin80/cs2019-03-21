845cf1891aa24fca40d3304508b5992ca3e8243b
chunikhin80 <k-o-r@tut.by>
2019-06-27 15:17:29 +0300

    lesson2

---
+++ src/by/it/Java2/Lesson3/task1.java
@@ -0,0 +1,18 @@
+package by.it.Java2.Lesson3;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class task1<тоже, Java> {
+    public static void main(String[] args) {
+       String s= "Дай пять.";
+       s+="Дай еще раз";
+        System.out.println(s);
+    }
+
+
+
+
+}
+
