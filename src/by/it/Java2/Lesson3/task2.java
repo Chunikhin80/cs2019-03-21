961e4aebdaf16077322ed37463c89c5d248a7352
chunikhin80 <k-o-r@tut.by>
2019-06-27 18:32:03 +0300

    lesson2

---
+++ src/by/it/Java2/Lesson3/task2.java
@@ -0,0 +1,11 @@
+package by.it.Java2.Lesson3;
+
+public class task2 {
+    public static void main(String[] args) {
+        String s=(":(  :(  :(  :( ");
+        System.out.println(s);
+        System.out.println("------------------");
+        String s1=s.replace(":(",":)");
+        System.out.println(s1);
+    }
+}
