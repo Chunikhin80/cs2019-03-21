4ac945756d201029bfe67d6f7af3b80e21af9b1a
chunikhin80 <k-o-r@tut.by>
2019-06-27 19:49:22 +0300

    lesson2

---
+++ src/by/it/Java2/Lesson3/task3.java
@@ -0,0 +1,12 @@
+package by.it.Java2.Lesson3;
+
+public class task3 {
+    public static void main(String[] args) {
+     String s="я ты он она оно они я";
+     boolean end=s.endsWith("я");
+        System.out.println(end);
+
+        boolean start=s.startsWith("я");
+        System.out.println(start);
+    }
+}
