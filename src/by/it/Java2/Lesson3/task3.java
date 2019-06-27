ffaf2da253a6d1f664c1458e9c06ad02014db6b9
chunikhin80 <k-o-r@tut.by>
2019-06-27 20:14:48 +0300

    lesson2

--- src/by/it/Java2/Lesson3/task3.java
+++ src/by/it/Java2/Lesson3/task3.java
@@ -2,11 +2,17 @@ package by.it.Java2.Lesson3;
 
 public class task3 {
     public static void main(String[] args) {
-     String s="я ты он она оно они я";
+     String s="я ты он она оно они ";
      boolean end=s.endsWith("я");
-        System.out.println(end);
+     boolean start=s.startsWith("я");
+        GetWordLineEqual(end, start);
 
-        boolean start=s.startsWith("я");
-        System.out.println(start);
+    }
+
+    private static void GetWordLineEqual(boolean end, boolean start) {
+        if ((end & start)==true)
+            System.out.println("true");
+        else
+            System.out.println("false");
     }
 }
---
+++ src/by/it/Java2/Lesson3/task4.java
@@ -0,0 +1,7 @@
+package by.it.Java2.Lesson3;
+
+public class task4 {
+    public static void main(String[] args) {
+
+    }
+}
