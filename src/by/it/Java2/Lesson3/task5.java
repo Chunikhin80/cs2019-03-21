608a539b73516f33f698c079be83b88685788f27
chunikhin80 <k-o-r@tut.by>
2019-06-30 22:16:02 +0300

    lesson3

--- src/by/it/Java2/Lesson3/task5.java
+++ src/by/it/Java2/Lesson3/task5.java
@@ -9,21 +9,15 @@ public class task5 {
         System.out.println("Введите Ваш текст:");
         String text = in.nextLine();
         text = text.trim();
-        int a1 = 0;
-        int a2 = 1;
         int word = 0;
 
-        int words = getWords(text, a1, a2, word);
-
+        int words=getWords(text,word);
         System.out.println(words);
     }
 
-    private static int getWords(String text, int a1, int a2, int word) {
+    private static int getWords(String text, int word) {
         for (int i = 0; i < text.length(); i++) {
-            String t = text.substring(a1, a2);
-            a1++;
-            a2++;
-            if (t.equals(" ")) {
+            if (text.charAt(i) == ' ' && text.charAt(i + 1) != ' ') {
                 word++;
             }
         }
