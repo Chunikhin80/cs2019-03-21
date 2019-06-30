d9ad7d75bffe2706375fb34d12b21e2e6f4851d6
chunikhin80 <k-o-r@tut.by>
2019-06-30 21:57:19 +0300

    lesson3

--- src/by/it/Java2/Lesson3/task7.java
+++ src/by/it/Java2/Lesson3/task7.java
@@ -6,11 +6,17 @@ public class task7 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите Ваше число:");
-        int num = in.nextInt();
+        long num = in.nextLong();
+
 
         String getString = String.format("%010d", num);
-        System.out.println(getString);
+            if (num > 9999999999L) {
+                System.out.printf("%010d", 0);
+            } else {
+                System.out.println(getString);
 
-        //System.out.printf("%010d",num);
+
+                //System.out.printf("%010d",num);
+            }
+        }
     }
-}
