70feba8642b5cb5d5e81d501105d1d9a07cbdb5c
chunikhin80 <k-o-r@tut.by>
2019-07-04 21:11:14 +0300

    lesson4

---
+++ src/by/it/Java2/Lesson4/task1.java
@@ -0,0 +1,65 @@
+package by.it.Java2.Lesson4;
+
+public class task1 {
+    public static void main(String[] args) {
+        TimeLength timeOne = new TimeLength(12, 24, 32);
+        TimeLength timeTwo = new TimeLength(25001);
+
+        printTimeLength(timeOne);
+        printTimeLength(timeTwo);
+        }
+
+    public static void printTimeLength(TimeLength time) {
+
+        System.out.println("time =    " + time);
+        System.out.println("seconds = " + time.getShareSeconds());
+
+    }
+}
+
+class TimeLength {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+
+    public TimeLength(int hours, int minutes, int seconds) {
+
+        this.hours = Math.abs(hours) % 24;
+        this.minutes = Math.abs(minutes) % 60;
+        this.seconds = Math.abs(seconds) % 60;
+    }
+
+    public TimeLength(int seconds) {
+        convertSecondsToTime(seconds);
+    }
+
+    public int getShareSeconds() {
+        return this.hours * 3600 + this.minutes * 60 + this.seconds;
+    }
+
+    private void convertSecondsToTime(int seconds) {
+        seconds = Math.abs(seconds);
+        this.hours = seconds / 3600 % 24;
+        seconds %= 3600;
+        this.minutes = seconds / 60;
+        this.seconds = seconds % 60;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d h. %d m. %d s", this.hours, this.minutes, this.seconds);
+    }
+}
